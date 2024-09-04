a0e5043d06140f8ce6223d9ba358becfc566fdb2
Serein <2075337935@qq.com>
2024-09-04 14:14:23 +0800

    fix

--- scripts/windows-publish.ps1
+++ scripts/windows-publish.ps1
@@ -1,23 +1,9 @@
-[CmdletBinding()]
-param (
-    [string] $archiveName
-)
-
 # 外部环境变量包括:
-# archiveName: ${{ matrix.qt_ver }}-${{ matrix.qt_arch }}
 # winSdkDir: ${{ steps.build.outputs.winSdkDir }}
 # winSdkVer: ${{ steps.build.outputs.winSdkVer }}
 # vcToolsInstallDir: ${{ steps.build.outputs.vcToolsInstallDir }}
 # vcToolsRedistDir: ${{ steps.build.outputs.vcToolsRedistDir }}
 # msvcArch: ${{ matrix.msvc_arch }}
-
-
-# winSdkDir: C:\Program Files (x86)\Windows Kits\10\ 
-# winSdkVer: 10.0.19041.0\ 
-# vcToolsInstallDir: C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Tools\MSVC\14.28.29333\ 
-# vcToolsRedistDir: C:\Program Files (x86)\Microsoft Visual Studio\2019\Enterprise\VC\Redist\MSVC\14.28.29325\ 
-# archiveName: 5.9.9-win32_msvc2015
-# msvcArch: x86
 
 $scriptDir = $PSScriptRoot
 $currentDir = Get-Location
@@ -25,7 +11,6 @@ Write-Host "currentDir" $currentDir
 Write-Host "scriptDir" $scriptDir
 
 function Main() {
-
     New-Item -ItemType Directory dist
     # 拷贝exe
     Copy-Item build\src\Release\* dist -Force -Recurse | Out-Null
@@ -43,7 +28,7 @@ function Main() {
     $sdkDll="{0}Redist\{1}ucrt\DLLs\{2}\*.dll" -f $env:winSdkDir.Trim(),$env:winSdkVer.Trim(),$env:msvcArch
     Copy-Item $sdkDll dist\
     # 打包zip
-    Compress-Archive -Path dist $archiveName'.zip'
+    Compress-Archive -Path dist 'sast-evento-'$env:msvcArch'.zip'
 }
 
 Main
